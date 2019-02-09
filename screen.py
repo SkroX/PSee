99b849ae396e735691e322a44fdec0894ca5f9c5
SkroX <shubhamcricketer.kumar27@gmail.com>
2019-02-09 15:47:04 +0530

    get screenshot

--- screen.py
+++ screen.py
@@ -1,2 +1,10 @@
-def get_screenshot():
-    pass
+import pyscreenshot as ps
+import matplotlib.image as img
+def getScreenShot():
+	image=ps.grab()
+	image.save('sa.png')
+	#image.show()
+	matrix=img.imread('sa.png')
+	return matrix
+getScreenShot()
+	
