c771f9fbc08103b490f8d50b170d2aff302352f2
Vansika Pareek <vansikapareek2001@gmail.com>
2019-02-10 11:54:07 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,23 +2,23 @@ import socket
 import sys
 import pygame
 
-HOST, PORT = "127.0.0.1", 9000
+HOST, PORT = "127.0.0.1", 9001
 data = " ".join(sys.argv[1:])
 screen = pygame.display.set_mode((1600, 900))
 pygame.display.set_caption('PSee')
 white = (255, 64, 64)
 
-def get_image_from_server(sock):
-    with open ('screennew.png', 'wb') as f:
-        size = int(sock.recv(18).decode('utf-8'))
-        print("size of image: ", size)
-        received = b''
-        while size > 0:
-            r = sock.recv(size)
-            size -= len(r)
-            received += r
-        print("Received: {}".format(len(received)))
-        f.write(received)
+def get_image_from_server(sock, file):
+    size = int(sock.recv(18).decode('utf-8'))
+    print("size of image: ", size)
+    received = b''
+    while size > 0:
+        r = sock.recv(size)
+        size -= len(r)
+        received += r
+    print("Received: {}".format(len(received)))
+    file.write(received)
+    file.seek(0)
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -26,12 +26,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
     sock.sendall(bytes(data + "\n", "utf-8"))
     # Receive data from the server and shut down
+    file = open('screennew.png', 'wb')
     while True:
-        get_image_from_server(sock)
+        get_image_from_server(sock, file)
         img = pygame.image.load('screennew.png')
         screen.fill((white))
         img = pygame.transform.scale(img, (1600, 900))
         screen.blit(img, (0,0))
         pygame.display.flip()
 print("Sent:     {}".format(data))
-
